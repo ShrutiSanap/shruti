336fe6194fe7cbf86d14a1e46eb85a127339c9f8
shrut <shrut@Shruti>
2024-06-27 22:34:59 +0530

    second java program

---
+++ DayOne2/src/Shruti/FourthProgram.java
@@ -0,0 +1,25 @@
+package Shruti;
+
+public class FourthProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=0, y=7;
+		int a=10;
+		int b=11;
+		
+		if (x>=y) {
+			System.out.println("true");
+			}else {
+				System.out.println("False");
+			}
+		if (! (a<b ) || (a==b)){
+			System.out.println("condition is true");
+			
+		} else {
+			System.out.println("Condition is false");
+		}
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ DayOne2/src/Shruti/ThirdProgram.java
@@ -0,0 +1,19 @@
+package Shruti;
+
+public class ThirdProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=22;
+		if (age>=18)
+		{
+			System.out.println("You are eligible");
+			
+		}
+		else {
+			System.out.println("You r not");
+		}
+		// TODO Auto-generated method stub
+	}
+
+}
